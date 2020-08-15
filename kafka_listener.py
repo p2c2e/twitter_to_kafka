7b24d617147dac8f294da41e9d7ad0dfc0bbc457
Sudharsan Rangarajan <sudharsan.rangarajan@publicissapient.com>
2020-08-15 19:40:34 +0530

    ArgParse for listener

--- kafka_listener.py
+++ kafka_listener.py
@@ -1,8 +1,14 @@
 import traceback
 from kafka import KafkaConsumer
+import argparse
 
-topic = "twitter"
-consumer = KafkaConsumer(topic, bootstrap_servers=['192.168.1.9:9092'],)
+parser = argparse.ArgumentParser(description='Listen and print from Kafka Topic')
+parser.add_argument('--kb', dest='broker', default="127.0.0.1:9092", help='BrokerIP:PORT (127.0.0.1:9092)')
+parser.add_argument('--topic', dest='topic', default="twitter", help='Kafka Topic (twitter)')
+         
+args = parser.parse_args()
+
+consumer = KafkaConsumer(args.topic, bootstrap_servers=[args.broker],)
 for message in consumer:
     print (message)
 
